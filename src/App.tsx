f6afaf7e10e95a24808fcab37644995116b75b8d
SergoJpg <sergokasheew@gmail.com>
2023-10-22 16:31:05 +0500

    первые потуги

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import PotatoShop from './components/PotatoShop';
 
-function App() {
+const App = () => {
+  const [state, setState] = useState({
+    totalPotatoes: 0,
+    shop: [
+      {
+        name: 'hoe',
+        image: '',
+        amount: 1,
+        startPotatoPerSec: 1,
+        startPrice: 15,
+        priceIncreaseByAmount: 5,
+        upgradeLevel: 2,
+      },
+      {
+        name: 'farm',
+        image: '',
+        amount: 0,
+        startPotatoPerSec: 10,
+        startPrice: 150,
+        priceIncreaseByAmount: 25,
+        upgradeLevel: 0,
+      },
+    ],
+  });
+
+  const addPotatoesPerSec = () => {
+    const newTotalPotatoes = state.shop.reduce((acc, el) => {
+      return el.startPotatoPerSec * 2 ** el.upgradeLevel * el.amount + acc;
+    }, 0);
+    console.log(state.totalPotatoes);
+
+    const newState = {
+      ...state,
+      totalPotatoes: newTotalPotatoes + state.totalPotatoes,
+    };
+
+    setState(newState);
+  };
+
+  useEffect(() => {
+    setInterval(() => {
+      console.log('1');
+      addPotatoesPerSec();
+    }, 1000);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='main-block'>
+        <div className='total-potatoes'>{state.totalPotatoes}</div>
+        <div className='potatoes-per-sec'></div>
+        <div className='potato'></div>
+        <div className='potatoes-per-click'></div>
+      </div>
+      <PotatoShop></PotatoShop>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/PotatoShop.tsx
@@ -0,0 +1,5 @@
+const PotatoShop = () => {
+  return <div className='shop'></div>;
+};
+
+export default PotatoShop;
